7aa30eb1b1dba523c8670dcce68a5c1b3af20967
Alex D. Garcia <alex19_fandbz@hotmail.com>
2022-11-09 14:05:28 -0600

    Precios de libros

--- DgBooksDB/DgBksModel.Designer.cs
+++ DgBooksDB/DgBksModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\MichaelGonzalez\Documents\UTTT\9no\repos\DgBooks\DgBooksDB\DgBksModel.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\alex1\OneDrive\Escritorio\Libreril\dg-bocks\DgBooksDB\DgBksModel.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
